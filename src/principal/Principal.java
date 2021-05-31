d7853ab82eab8089d19dd212c39b8acd90e8e8e8
Daniel Calderon <danycalvir@gmail.com>
2021-05-31 17:08:57 -0600

    mvc en barra de menu

---
+++ src/principal/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+import analizador.Editor;
+import controladores.ControladorEditor;
+import modelos.ModeloEditor;
+
+/**
+ *
+ * @author danyc
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        Editor vista = new Editor();
+        ModeloEditor modelo = new ModeloEditor();
+        ControladorEditor controlador;
+        controlador = new ControladorEditor(vista, modelo);
+        vista.setVisible(true);
+    }
+}
